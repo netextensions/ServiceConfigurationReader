0442b05c7616479622278f240d5041e34906967a
Csaba Salat <csalat@globalblue.com>
2020-04-10 15:54:46 +0200

    ServiceConfigurationReader

---
+++ Source/ServiceConfigurationReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ServiceConfigurationReader
+{
+    public class ServiceConfigurationReader
+    {
+        public IConfiguration Configuration(params string[] jsonFileNames)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(((Func<string>)(() =>
+                            {
+                                var processModule = Process.GetCurrentProcess().MainModule;
+                                return processModule != null ? Path.GetDirectoryName(processModule.FileName) : throw new ApplicationException("processModule is not found");
+                            }
+                        )
+                    )())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+                .AddEnvironmentVariables();
+
+            foreach (var fileName in jsonFileNames)
+                builder.AddJsonFile(fileName);
+
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Test/ServiceConfigurationReaderUnitTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceConfigurationReader.Test
+{
+    [TestClass]
+    public class ServiceConfigurationReaderUnitTest
+    {
+        //C:\Program Files\dotnet\appsettings.json
+        [TestMethod]
+        public void ServiceConfigurationReaderTest()
+        {
+            var configurationReader = new ServiceConfigurationReader();
+            var configuration = configurationReader.Configuration();
+        }
+    }
+}
